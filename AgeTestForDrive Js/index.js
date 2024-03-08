6d00c83dd6e9fd2f674ef190dc6261b7838e8b3f
Amir For IT Solution <160061309+aamirfouad@users.noreply.github.com>
2024-03-08 16:38:56 +0300

    Add files via upload

---
+++ AgeTestForDrive Js/index.js
@@ -0,0 +1,10 @@
+let Age =prompt('How Old Are You?');
+
+if(Age>=18){
+    console.log(`You are old enough to drive.`)
+}
+else if(Age<18){
+
+    console.log(`You are left with ${18 - Age} years to drive.`)
+
+}
